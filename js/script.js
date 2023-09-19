47e936907cb432e3c132d1df319603183190b1e8
dayalsaha <dayalsaha1234567@gmail.com>
2023-09-19 18:16:17 +0600

    responsive header

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+//mobile responsive menue
+
+let menu = document.querySelector('#menu_ico');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('bx-x');
+    menu.classList.toggle('open');
+};
